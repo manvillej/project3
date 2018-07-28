b099b5490a0b23c0944009ede4b43718d7e8e6e6
Jeff Manville <jeffmanvillejr@gmail.com>
2018-07-28 15:07:42 +0000

    Adding routes to Orders application

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,31 @@
+# TODO: module docstring
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    # TODO: index docstring
+
+    return HttpResponse("Project 3: Index TODO")
+
+
+def register(request):
+    # TODO: register docstring
+    return HttpResponse("Project 3: Register TODO")
+
+
+def login(request):
+    # TODO: login docstring
+    return HttpResponse("Project 3: Login TODO")
+
+
+def logout(request):
+    # TODO: logout docstring
+    return HttpResponse("Project 3: Logout TODO")
+
+
+def menu(request):
+    # TODO: menu docstring
+    return HttpResponse("Project 3: Menu TODO")
+
