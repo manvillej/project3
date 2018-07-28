61a9d20f860c43fedfdc7c1bebd301c710d6dccb
Jeff Manville <jeffmanvillejr@gmail.com>
2018-07-28 20:46:45 +0000

    added user registration form

--- orders/views.py
+++ orders/views.py
@@ -1,31 +1,69 @@
 # TODO: module docstring
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from django.template import loader
+from .forms import UserForm
 
 # Create your views here.
 def index(request):
     # TODO: index docstring
+    template = loader.get_template("orders/index.html")
 
-    return HttpResponse("Project 3: Index TODO")
-
-
-def register(request):
-    # TODO: register docstring
-    return HttpResponse("Project 3: Register TODO")
-
-
-def login(request):
-    # TODO: login docstring
-    return HttpResponse("Project 3: Login TODO")
-
-
-def logout(request):
-    # TODO: logout docstring
-    return HttpResponse("Project 3: Logout TODO")
+    # context for passing user and cart info.
+    context = {}
+    return HttpResponse(template.render(context, request))
 
 
 def menu(request):
     # TODO: menu docstring
     return HttpResponse("Project 3: Menu TODO")
+
+class UserFormView(View):
+    """"""
+    # TODO: UserFormView class docstring
+    form_class = UserForm
+    template_name = "orders/registration_form.html"
+
+    def get(self, request):
+        """display a blank form"""
+        # TODO: UserFormView.get docstring
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        """Process form data"""
+        # TODO: UserFormView.post docstring
+        form = self.form_class(request.POST)
+
+        if(form.is_valid()):
+            user = form.save(commit=False)
+
+            # cleaned normalized data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+
+            user.username = username
+            user.email = email
+
+            # hash the password
+            user.set_password(password)
+
+            user.save()
+
+            # returns user object if credentials are correct
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                 if user.is_active:
+                     login(request, user)
+                     return redirect('index')
+
+        return render(request, self.template_name, {'form': form})
+
+
+
 
--- pinocchios/settings.py
+++ pinocchios/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'pinocchios.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'orders/templates'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
