c8b7b7d27617933036b496a2730ce1d9b2df45e6
Jeff Manville <jeffmanvillejr@gmail.com>
2018-07-29 22:48:06 +0000

    added ItemType, Item, Topping, and Size models

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import ItemType, Item
+from .models import ItemType, Item, Size, Topping
 
 # Register your models here.
 admin.site.register(ItemType)
 admin.site.register(Item)
+admin.site.register(Size)
+admin.site.register(Topping)
--- orders/models.py
+++ orders/models.py
@@ -3,7 +3,7 @@ import uuid
 
 # Create your models here.
 class ItemType(models.Model):
-    # TODO: itemtype docstring
+    # TODO: ItemType docstring
     name = models.CharField(max_length=32)
 
     def __str__(self):
@@ -11,12 +11,52 @@ class ItemType(models.Model):
 
 
 class Item(models.Model):
-    # TODO: item docstring
-    item_type = models.ForeignKey(ItemType, on_delete=models.CASCADE, related_name="ItemTypes")
+    # TODO: Item docstring
+    item_type = models.ForeignKey(ItemType, on_delete=models.CASCADE, related_name="ItemType")
     name = models.CharField(max_length=64)
     description = models.CharField(max_length=400)
 
     def __str__(self):
         return f'{self.item_type}:{self.name}'
+
+
+class Topping(models.Model):
+    # TODO: Topping docstring
+    item_type = models.ForeignKey(ItemType, on_delete=models.CASCADE, blank=True, null=True, )
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, blank=True, null=True, )
+    name = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        if(self.item):
+            return f'{self.item} - {self.name} - {self.price}'
+        elif(self.item_type):
+            return f'{self.item_type} - {self.name} - ${self.price}'
+        else:
+            return 'Error, topping requires item or item_type'
+
+
+class Size(models.Model):
+    # TODO: Size docstring
+    small = 'sm'
+    regular = 'rg'
+    large = 'lg'
+
+    SIZES = (
+        (small, 'Small'),
+        (regular, 'Regular'),
+        (large, 'Large'),
+    )
+
+    name = models.CharField(
+        max_length=2,
+        choices=SIZES,
+        default=regular,
+    )
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name="Item")
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.item} - {self.name} - {self.price}'
 
 
